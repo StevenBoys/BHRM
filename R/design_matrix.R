ef9041a3d5b7d202a042a57d8606aee3de294bdb
Bowen Lei <bowen.lei@stat.tamu.edu>
2020-12-18 22:03:01 -0600

    Add two example datasets for the package.

---
+++ R/design_matrix.R
@@ -0,0 +1,8 @@
+#' Potential covariates of 30 countries (noisy version)
+#'
+#' A dataframe contains 20 potential factors for COVID-19 growth trajectory of 30 countries.
+#'
+#' @format A data frame with 30 rows and 22 variables:
+#'
+#' @source c(\url{https://data.worldbank.org/}, \url{https://apps.who.int/gho/data/node.main})
+"design_matrix"
